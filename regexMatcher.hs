96d224d88ae4e8bdc8b12477649ca24ba4778369
James A Keene <outofthemadness@gmail.com>
2015-03-18 17:48:33 -0400

    delete redundant parens

--- regexMatcher.hs
+++ regexMatcher.hs
@@ -13,8 +13,8 @@ match :: Regex -> String -> Bool
 match Any                 _  = True
 match Empty               _  = False
 match (Repetition r1)     s  = any (match r1) (take (length s) $ iterate tail s)
-match r@(Sequence (Repetition r1) r2) s = match r2 s || ((null s) && charMatch r1 (head s) && match r (tail s))
-match r@(Primitive c) s = charMatch r (head s)
+match r@(Sequence (Repetition r1) r2) s = match r2 s || (null s && charMatch r1 (head s) && match r (tail s))
+match r@(Primitive _) s = charMatch r (head s)
 match _                   "" = False
 match (Sequence r1 Empty) s  = match r1 s
 match (Sequence r1 r2)    s  = match r1 (takeWhile (not .charMatch r2)  s) && match r2 (head $ dropWhile (match r1) $ iterate tail s)
