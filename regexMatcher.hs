ee2009ba4b3c07fdf2eecc1bcd5ab5cef2be27ea
James A Keene <outofthemadness@gmail.com>
2015-03-18 17:46:22 -0400

    rename matchChar to charMatch

--- regexMatcher.hs
+++ regexMatcher.hs
@@ -13,21 +13,24 @@ match :: Regex -> String -> Bool
 match Any                 _  = True
 match Empty               _  = False
 match (Repetition r1)     s  = any (match r1) (take (length s) $ iterate tail s)
-match (Repetition r1)     "" = True
+match r@(Sequence (Repetition r1) r2) s = match r2 s || ((null s) && charMatch r1 (head s) && match r (tail s))
+match r@(Primitive c) s = charMatch r (head s)
 match _                   "" = False
 match (Sequence r1 Empty) s  = match r1 s
--- match (Sequence r1 r2)    s  = case (r1,r2) of 
---                                                ((Repetition Any),_)
-match (Sequence r1 r2)    s  = match r1 (takeWhile (not .matchChar r2)  s) && match r2 (head $ dropWhile (match r1) $ iterate tail s)
-match (Primitive c)       s  = [c] == s
+match (Sequence r1 r2)    s  = match r1 (takeWhile (not .charMatch r2)  s) && match r2 (head $ dropWhile (match r1) $ iterate tail s)
 
-matchChar :: Regex -> Char -> Bool
-matchChar Any _  = True
-matchChar Empty _ = False
-matchChar (Primitive r) c = r == c 
-matchChar (Sequence r1 Empty) s = matchChar r1 s
-matchChar (Sequence r1 r2) s = (matchChar r1 s) && (matchChar r2 s)
-matchChar (Repetition Any) _ = False
+charMatch :: Regex -> Char -> Bool
+charMatch Any _  = True
+charMatch Empty _ = False
+charMatch (Primitive r) c = r == c 
+-- charMatch (Sequence r1 Empty) s = charMatch r1 s
+-- -- charMatch (Sequence r1 r2) s = (charMatch r1 s) && (charMatch r2 s)
+-- charMatch (Repetition Any) _ = False
+
+-- charMatch :: String -> String -> Bool
+-- charMatch s p = case (x@(head s),y@(head p)) of
+--                     x == '.' -> True
+--                     head x == '*' -> 
 
 main :: IO ()
 main = do 
