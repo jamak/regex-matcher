26e07823eca023a80055c676658ec3df8eb2b562
James A Keene <outofthemadness@gmail.com>
2015-03-18 15:51:20 -0400

    match should work against sequences correctly now

--- regexMatcher.hs
+++ regexMatcher.hs
@@ -9,12 +9,13 @@ build (c:'*':cs) = Sequence (Repetition (Primitive c)) (build cs)
 build (c:cs) = Sequence (Primitive c) (build cs)
 
 match :: Regex -> String -> Bool
-match Any              _  = True
-match Empty            _  = False
-match _                "" = False
-match (Sequence r1 r2) s  = match r1 s && match r2 s -- second match is against s - the section that matched against r1
-match (Repetition r1)  s  = any (match r1) (take (length s) $ iterate tail s)
-match (Primitive c)    s  = c == head s
+match Any                 _  = True
+match Empty               _  = False
+match _                   "" = False
+match (Sequence r1 Empty) s  = match r1 s
+match (Sequence r1 r2)    s  = match r1 s && match r2 (head $ dropWhile (match r1) $ iterate tail s)
+match (Repetition r1)     s  = any (match r1) (take (length s) $ iterate tail s)
+match (Primitive c)       s  = c == head s
 
 main :: IO ()
 main = do 
