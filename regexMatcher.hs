44021b2aeddaf9c627d7fce6e01f73028c948aff
James A Keene <outofthemadness@gmail.com>
2015-03-18 18:00:48 -0400

    remove charMatch, match works now
    charMatch was always an intermediate function, it was only intended to bootstrap
    the behavior I wanted from match, and now that match works correctly, it's not
    necessary

--- regexMatcher.hs
+++ regexMatcher.hs
@@ -10,27 +10,27 @@ build (c:'*':cs) = Sequence (Repetition (Primitive c)) (build cs)
 build (c:cs) = Sequence (Primitive c) (build cs)
 
 match :: Regex -> String -> Bool
-match Any                 _  = True
-match Empty               _  = False
-match (Repetition r1)     s  = any (match r1) (take (length s) $ iterate tail s)
-match r@(Sequence (Repetition r1) r2) s = match r2 s || (null s && charMatch r1 (head s) && match r (tail s))
-match r@(Primitive _) s = charMatch r (head s)
-match _                   "" = False
-match (Sequence r1 Empty) s  = match r1 s
-match (Sequence r1 r2)    s  = match r1 (takeWhile (not .charMatch r2)  s) && match r2 (head $ dropWhile (match r1) $ iterate tail s)
-
-charMatch :: Regex -> Char -> Bool
-charMatch Any _  = True
-charMatch Empty _ = False
-charMatch (Primitive r) c = r == c 
--- charMatch (Sequence r1 Empty) s = charMatch r1 s
--- -- charMatch (Sequence r1 r2) s = (charMatch r1 s) && (charMatch r2 s)
--- charMatch (Repetition Any) _ = False
+match Any _              = True
+match Empty _            = True
+match (Primitive c) s    = [c] == s
+match (Sequence r1 r2) s = or [(match r1 u1) && (match r2 u2) | (u1,u2) <- split s]
+match (Repetition r1) s  = or $ (match r1 s):[and [match r1 s1 | s1 <- ps] | ps <- partitions s]
 
 -- charMatch :: String -> String -> Bool
 -- charMatch s p = case (x@(head s),y@(head p)) of
 --                     x == '.' -> True
 --                     head x == '*' -> 
+
+-- All possible ways to split a list in two
+split :: [a] -> [([a], [a])]
+split []     = [([], [])]
+split (c:cs) = ([], c:cs):[(c: s1, s2) | (s1, s2) <- split cs]
+
+-- returns a list of all the the possible ways to split up the list
+partitions :: [a] -> [[[a]]]
+partitions []     = [[]]
+partitions [c]    = [[[c]]]
+partitions (c:cs) = concat [[(c:p):ps, [c]:p:ps]| p:ps <- partitions cs]
 
 main :: IO ()
 main = do 
